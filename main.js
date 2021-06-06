607fd45222324ea6975bceeed2bf9e56f61889cb
Dominic Hoogkamer <39436759+Scaredtodev@users.noreply.github.com>
2021-06-06 16:43:17 +0200

    Added eslint,stylelint,prettier and husky

--- main.js
+++ main.js
@@ -1,80 +1,85 @@
-import './style.css'
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default class Sketch {
-    constructor(options) {
-        this.time = 0;
-        this.container = options.dom;
-        this.scene = new THREE.Scene();
+  constructor(options) {
+    this.time = 0;
+    this.container = options.dom;
+    this.scene = new THREE.Scene();
 
-        this.width = this.container.offsetWidth;
-        this.height = this.container.offsetHeight;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
 
-        this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
-        this.camera.position.z = 1;
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
 
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-        this.renderer.setSize( this.width, this.height );
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-        this.container.appendChild( this.renderer.domElement );
+    this.container.appendChild(this.renderer.domElement);
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
-        this.resize();
-        this.setupResize();
-        this.setupFullscreen();
-        this.addObjects();
-        this.render();
-    }
+    this.resize();
+    this.setupResize();
+    this.setupFullscreen();
+    this.addObjects();
+    this.render();
+  }
 
-    setupResize() {
-        window.addEventListener('resize', this.resize.bind(this));
-    }
+  setupResize() {
+    window.addEventListener("resize", this.resize.bind(this));
+  }
 
-    setupFullscreen() {
-        window.addEventListener('dblclick', this.fullscreen.bind(this));
-    };
+  setupFullscreen() {
+    window.addEventListener("dblclick", this.fullscreen.bind(this));
+  }
 
-    fullscreen() {
-        if (!document.fullscreenElement) {
-            // TODO: Set Canvas to fullscreen
-            // canvas.requestFullscreen();
-        } else {
-            // document.exitFullscreen();
-        }
-    };
+  //   fullscreen() {
+  //     if (!document.fullscreenElement) {
+  //       // TODO: Set Canvas to fullscreen
+  //       // canvas.requestFullscreen();
+  //     } else {
+  //       // document.exitFullscreen();
+  //     }
+  //   }
 
-    resize() {
-        this.width = this.container.offsetWidth;
-        this.height = this.container.offsetHeight;
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
 
-        this.camera.aspect = this.width / this.height;
-        this.camera.updateProjectionMatrix();
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
 
-        this.renderer.setSize( this.width, this.height );
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    }
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
 
-    addObjects() {
-        this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-        this.material = new THREE.MeshNormalMaterial();
+  addObjects() {
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
 
-        this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.scene.add( this.mesh );
-    }
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
 
-    render() {
-        this.time += 0.05;
-        this.mesh.rotation.x = this.time / 2000;
-        this.mesh.rotation.y = this.time / 1000;
+  render() {
+    this.time += 0.05;
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
 
-        this.renderer.render( this.scene, this.camera );
-        window.requestAnimationFrame(this.render.bind(this));
-    }
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
 }
 
-new Sketch({
-    dom: document.getElementById('container')
+Sketch({
+  dom: document.getElementById("container"),
 });
